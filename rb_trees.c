84c79ecc1601955f74637d15a7daa54655e9899f
Sarthak Jain <332Sarthak22@gmail.com>
2021-11-09 17:44:54 +0530

    minor bug fixes

--- rb_trees.c
+++ rb_trees.c
@@ -217,9 +217,10 @@ struct RBNode *insertFixUp(struct RBNode *tree, struct RBNode *node_to_fix)
             else if (node_to_fix == parentNode->right) // forms a triangle
             {
                 node_to_fix = parentNode;
+                tree = left_rotation(tree, node_to_fix);
+
                 parentNode = parentOf(node_to_fix);
                 grandParentNode = grandParentOf(node_to_fix);
-                tree = left_rotation(tree, node_to_fix);
 
                 parentNode->node_color = BLACK;
                 grandParentNode->node_color = RED;
@@ -248,9 +249,10 @@ struct RBNode *insertFixUp(struct RBNode *tree, struct RBNode *node_to_fix)
             else if (node_to_fix == parentNode->left)
             {
                 node_to_fix = parentNode;
+                tree = right_rotation(tree, node_to_fix);
+
                 parentNode = parentOf(node_to_fix);
                 grandParentNode = grandParentOf(node_to_fix);
-                tree = right_rotation(tree, node_to_fix);
 
                 parentNode->node_color = BLACK;
                 grandParentNode->node_color = RED;
